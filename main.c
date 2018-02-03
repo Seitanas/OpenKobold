1321e9e2893823a8c5f579c2681691fb33174f59
Tadas Ustinavičius <tadas@ring.lt>
2018-02-03 23:57:25 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
  * Created on Ketvirtadienis, 2017, Gruodžio 21, 09.10
  */
 #pragma config FOSC = INTRCIO   // Oscillator Selection bits (INTOSC oscillator: I/O function on GP4/OSC2/CLKOUT pin, I/O function on GP5/OSC1/CLKIN)
-#pragma config WDTE = ON      // Watchdog Timer Enable bit (WDT disabled)
+#pragma config WDTE = ON      // Watchdog Timer Enable bit (WDT enabled)
 #pragma config PWRTE = OFF      // Power-Up Timer Enable bit (PWRT disabled)
 #pragma config MCLRE = OFF      // GP3/MCLR pin function select (GP3/MCLR pin function is digital I/O, MCLR internally tied to VDD)
 #pragma config BOREN = OFF      // Brown-out Detect Enable bit (BOD disabled)
